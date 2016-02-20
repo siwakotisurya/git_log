c310656a4b42a5a417ce3810f283f9ba032f180e
siwakotisurya <siwakotisurya@outlook.com>
2016-02-20 20:25:24 +0545

    grab git log of today

--- log_tracker.rb
+++ log_tracker.rb
@@ -2,14 +2,12 @@ class LogTracker
 
 	attr_accessor :start_time
 
-	include LogFormatter
-
 	def initialize
 		@start_time = start_time
 	end
 
 	def git_log
-		
+		`git log --after="yesterday"`
 	end
 
 end
