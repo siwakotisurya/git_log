ccf6e336ff39ffc4f442dbfd9e7e0cda2d0ab908
siwakotisurya <siwakotisurya@outlook.com>
2016-02-21 02:11:44 +0545

    in log_tracker.rb define two methods for parsing task json and taking user_input

--- log_tracker.rb
+++ log_tracker.rb
@@ -1,6 +1,10 @@
 class LogTracker 
 
+	attr_accessor :user_choice
+
 	require './modules/log_formatter'
+
+	require "pry"
 
 	DIR_NAME =  `git rev-parse --show-toplevel`
 
@@ -9,14 +13,37 @@ class LogTracker
 	def initialize
 		repo_name = DIR_NAME.split("\n").join("").split("/")
 		@git_log = `git log --pretty=format:'{%n  "commit": "%H",%n  "author": "%an <%ae>",%n  "date": "%ad",%n  "title": "%s",%n "description": "%b", %n "project_name": "#{repo_name.pop}"%n},' --since="yesterday"` 
+		@tasks_array = Array.new
 	end
 
 	def output_log
 		json_data = self.format_json(@git_log)
 	end
 
+	def execute_command
+	end
+
+	def parse_json
+		tasks = ["bug fixing", "development"]
+		tasks.each_with_index do |value, key|
+			task_hash = Hash.new
+			task_hash[key] = value
+			@tasks_array.push(task_hash)
+		end
+	end
+
+	def user_data
+		binding.pry
+		@tasks_array.each do |tasks|
+			tasks.each do |key, value|
+				puts key
+				puts value
+			end
+		end
+	end
+
 end
 
 git = LogTracker.new
-
-git.output_log
+git.parse_json
+git.user_data
